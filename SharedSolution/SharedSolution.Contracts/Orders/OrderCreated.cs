1b6b503de8ea7352358d85f5abf83a94263f251c
Nguyen-Son-Tung <tung.sonnguyen@infodation.vn>
2024-12-19 18:04:43 +0700

    Shared solution

---
+++ SharedSolution/SharedSolution.Contracts/Orders/OrderCreated.cs
@@ -0,0 +1,15 @@
+﻿namespace SharedSolution.Contracts.Orders
+{
+    public record OrderCreated
+    {
+        public required string OrderId { get; set; }
+        public required string CustomerId { get; set; }
+        public IEnumerable<OrderDetail> Details { get; set; } = [];
+    }
+
+    public record OrderDetail
+    {
+        public required string ProductId { get; set; }
+        public required int Quantity { get; set; }
+    }
+}
